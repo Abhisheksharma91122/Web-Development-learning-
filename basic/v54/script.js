9335525de05c3e7718aae97b1d2efffa15e74cb5
Abhishek <abhisheksharma270820@gmail.com>
2024-12-05 21:54:10 +0530

    Learn javascript basics and variable and datatype

---
+++ basic/v54/script.js
@@ -0,0 +1,19 @@
+alert("hello world")
+console.log("code is Running...")
+console.log("still Running...")
+console.log("Running...")
+
+var a = prompt("Enter a number: ")
+console.log("your number is : " + a)
+
+var isture = confirm("Are you sure you want to enter to this website : ")
+
+
+if(isture){
+    console.log("welcome")
+}
+else{
+    console.log("bad luck!")
+}
+document.title = "welcome to java script"
+document.body.style.backgroundColor = "pink"
---
+++ basic/v55/script.js
@@ -0,0 +1,35 @@
+console.log("welcome")
+
+let a = 5;
+var b = 10;
+var c = "Abhishek"
+console.log(a + b)
+console.log(typeof a, typeof b , typeof c)
+{
+    let a =20;
+    console.log(a)
+}
+console.log(a)
+// // var 45a = 45; this is not Allowed
+// const _abhi = 45;
+// _abhi = _abhi + 1;
+// Not Allowed 
+let x = 45;
+let y ="Abhishek"
+let p = 45.2;
+const d = true;
+let g = undefined;
+let h = null;
+
+console.log(y,x,p,g,h)
+console.log(typeof y,typeof x,typeof p,typeof g,typeof h)
+ 
+
+let abhi ={
+    name: "abhishek sharma",
+    "Roll no" : 72,
+    "is_handsome": true,
+}
+console.log(abhi)
+abhi.salary = "45000 cores"
+console.log(abhi)
