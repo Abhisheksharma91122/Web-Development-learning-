ef67d12bbaba888781704df71841280f5d661e85
Abhishek <abhisheksharma270820@gmail.com>
2025-03-25 22:07:56 +0530

    Install Next.js, add About & Contact pages, verify routing, and explore file patterns

---
+++ basic/v121/first/app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <div className="text-xl">Problem Solved by Next.js</div>
+      <ul className='text-blue-700'>
+        <li>Full Stack Solution</li>
+        <li>File based Routing</li>
+        <li>Additional Features like router from next/navigation</li>
+        <li>Optimized rendering</li>
+      </ul>
+    </div>
+  )
+}
+
+export default page
